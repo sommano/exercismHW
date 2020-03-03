934c9c25f6d506d8081f3f7fd0d4a497efb45765
sommano@gmail.com <sommano@gmail.com>
2020-03-02 20:19:09 -0600

    commits

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -2,9 +2,6 @@
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+export const colorCode = color => COLORS.indexOf(color);
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
-};
-
-export const COLORS = undefined;
