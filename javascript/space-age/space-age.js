9ba7cb9ceac9af889319a5b8d3b601b295e9a61a
sommano@gmail.com <sommano@gmail.com>
2020-03-06 22:14:50 -0600

    commit

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet,ageSeconds) => {
+  const Planets = {
+    "mercury" : 0.2408467,
+    "venus" : 0.61519726,
+    "earth" : 1.0,
+    "mars" : 1.8808158,
+    "jupiter" : 11.862615,
+    "saturn" : 29.447498,
+    "uranus" : 84.016846,
+    "neptune" : 164.79132
+  }
+  const secsOnEarth = 31557600;   
+  let age = ageSeconds / secsOnEarth;
+
+  return Number((age / Planets[planet.toLowerCase()]).toFixed(2));
 };
