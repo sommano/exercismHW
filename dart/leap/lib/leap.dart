e1e82aaf3fe034000e7f027354920ffd5bee3cf0
sommano@gmail.com <sommano@gmail.com>
2020-03-09 10:42:08 -0500

    commit :

---
+++ dart/leap/lib/leap.dart
@@ -0,0 +1,14 @@
+class Leap {
+  
+  bool leapYear(int year) {
+    if ((year % 4) == 0) {
+      return true;
+    } else if ((year % 400) == 0) {
+      return true;
+    } else if ((year % 100) == 0) {
+      return false;
+    } else if (year >= 0) {
+      return false;
+    }
+  }
+}
