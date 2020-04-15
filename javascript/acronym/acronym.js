8d9fb4f33718d1124943434c5b4b118dd6c81c55
Sommano Phivilay <sommano@gmail.com>
2020-04-15 19:36:23 -0400

    Update acronym.js

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -2,6 +2,7 @@
 // This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+//
 
 export const parse = () => {
   throw new Error("Remove this statement and implement this function");
