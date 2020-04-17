162cdf319189614266e7ce16fc2455782e94b3ec
Sommano Phivilay <sommano@gmail.com>
2020-04-16 21:37:35 -0400

    Update acronym.dart

--- dart/acronym/lib/acronym.dart
+++ dart/acronym/lib/acronym.dart
@@ -1,3 +1,11 @@
 class Acronym {
-  // Put your code here
+
+  String abbreviate(String name) =>
+    name
+      .replaceAll("_", "")
+      .split(new RegExp(r"[\s-]"))
+      .map((word) => word.length > 1 ? word[0] : word)
+      .map((letter) => letter.toUpperCase())
+      .where((item) => item.contains(new RegExp(r"[A-Z]")))
+      .reduce((result, item) => result + item);
 }
