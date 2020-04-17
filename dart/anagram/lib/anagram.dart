39ab865ea75ee78caafb59ec691ef1744a3c886c
Sommano Phivilay <sommano@gmail.com>
2020-04-17 15:42:13 -0400

    Update anagram.dart

--- dart/anagram/lib/anagram.dart
+++ dart/anagram/lib/anagram.dart
@@ -1,3 +1,25 @@
 class Anagram {
-  // Put your code here
+  List<String> findAnagrams(String word, List<String> cadinates) {
+    var result = new List<String>();
+
+    for (final cadinate in cadinates) {
+      if (word.length == cadinate.length &&
+          isAnagram(word.toLowerCase(), cadinate.toLowerCase())) {
+        result.add(cadinate);
+      }
+    }
+
+    return result;
+  }
+
+  bool isAnagram(String word, String cadinate) {
+    if (word == cadinate) {
+      return false;
+    }
+
+    var cadinateRunes = cadinate.runes.toList();
+    word.runes.forEach((char) => cadinateRunes.remove(char));
+
+    return cadinateRunes.length == 0;
+  }
 }
