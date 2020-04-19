91178e557b5d1dcf13aa99f8b188fcfbcd873922
Sommano Phivilay <sommano@gmail.com>
2020-04-19 12:09:01 -0400

    Update beer_song.dart

--- dart/beer-song/lib/beer_song.dart
+++ dart/beer-song/lib/beer_song.dart
@@ -1,3 +1,26 @@
 class BeerSong {
-  // Put your code here
+  List<String> verse(int n) {
+    switch (n) {
+      case 0:
+        return ["No more bottles of beer on the wall, no more bottles of beer.", "Go to the store and buy some more, 99 bottles of beer on the wall."];
+      case 1:
+        return ["1 bottle of beer on the wall, 1 bottle of beer.", "Take it down and pass it around, no more bottles of beer on the wall."];
+      case 2:
+        return ["2 bottles of beer on the wall, 2 bottles of beer.", "Take one down and pass it around, 1 bottle of beer on the wall."];
+      default:
+        return ["$n bottles of beer on the wall, $n bottles of beer.", "Take one down and pass it around, ${n - 1} bottles of beer on the wall."];
+    }
+  }
+
+  List<String> recite(int start, int n) {
+      List<String> s = [];
+      int end = start - n + 1;
+      if (start > 100 || start < 0 || end > 100 || end < 0) return s;
+      for (var i = start; i > end; i--) {
+          s.addAll(verse(i));
+          s.add('');
+      }
+      s.addAll(verse(end));
+      return s;
+  }
 }
