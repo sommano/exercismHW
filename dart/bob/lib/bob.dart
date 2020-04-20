d24452e4b6f5cb23a6408c40e8a793b8aec0ba99
Sommano Phivilay <sommano@gmail.com>
2020-04-20 10:43:44 -0400

    Update bob.dart

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,3 +1,14 @@
 class Bob {
-  // Put your code here
+  String response(String sentence) {
+    final String trimmedSentence = sentence.trim();
+    if (trimmedSentence.isEmpty) return "Fine. Be that way!";
+    if (isUpper(trimmedSentence) && trimmedSentence.endsWith("?")) return "Calm down, I know what I'm doing!";
+    if (isUpper(trimmedSentence)) return "Whoa, chill out!";
+    if (trimmedSentence.endsWith("?")) return "Sure.";
+    return "Whatever.";
+  }
+
+  static bool isLetter(String char) => new RegExp(r'[a-zA-Z]').hasMatch(char);
+  static bool isUpper(String s) => s.split('').any(isLetter) && s == s.toUpperCase();
+
 }
