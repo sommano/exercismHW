515a1219a1f14de07c7126de437a6d0eb6ad7d80
Sommano Phivilay <sommano@gmail.com>
2020-04-21 13:08:24 -0400

    Update darts.dart

--- dart/darts/lib/darts.dart
+++ dart/darts/lib/darts.dart
@@ -1,3 +1,10 @@
 class Darts {
-  // Put your code here
+  int score(double x, double y) {
+    double dist = sqrt(pow(x, 2) + pow(y, 2));
+
+    if (dist <= 1) return 10;
+    if (dist <= 5) return 5;
+    if (dist <= 10) return 1;
+    return 0;
+  }
 }
