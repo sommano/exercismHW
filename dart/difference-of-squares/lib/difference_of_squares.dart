c024245d81dadd2ea72c1ff7ea2a2bf461ac1c3c
Sommano Phivilay <sommano@gmail.com>
2020-04-23 12:47:45 -0500

    Update difference_of_squares.dart

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,8 @@
 class DifferenceOfSquares {
-  // Put your code here
+  // Put your code here  list(val) => new Iterable.generate(val, (i) => i + 1);
+  square(val) => val * val;
+  sum(left, right) => left + right;
+  squareOfSums(int val) => square(list(val).reduce(sum));
+  sumOfSquares(int val) => list(val).map(square).reduce(sum);
+  difference(int val) => squareOfSums(val) - sumOfSquares(val);
 }
