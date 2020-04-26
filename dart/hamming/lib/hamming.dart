c9a4cb2fdf0890712eca5b8916f688e816aa4113
Sommano Phivilay <sommano@gmail.com>
2020-04-26 11:50:43 -0500

    Update hamming.dart

--- dart/hamming/lib/hamming.dart
+++ dart/hamming/lib/hamming.dart
@@ -1,3 +1,10 @@
 class Hamming {
-  // Put your code here
+  int distance(String str1, String str2) {
+    int count = 0;
+    int i;
+    for (i = 0; i < str1.length; i++) {
+      if (str1[i] != str2[i]) count++;
+    }
+    return count;
+  }
 }
