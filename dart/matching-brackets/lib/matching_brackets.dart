b326c767f3ed23f97a915a5672def1497c2fa9e9
Sommano Phivilay <sommano@gmail.com>
2020-04-28 11:03:09 -0500

    Update matching_brackets.dart

--- dart/matching-brackets/lib/matching_brackets.dart
+++ dart/matching-brackets/lib/matching_brackets.dart
@@ -1,3 +1,19 @@
 class MatchingBrackets {
-  // Put your code here
+Map<String, String> m = Map.fromEntries([
+    MapEntry(')', '('),
+    MapEntry('}', '{'),
+    MapEntry(']', '[')
+  ]);
+
+  bool isPaired(String s) {
+    List<String> open = [];
+    for (var i = 0; i < s.length; i++) {
+      if (m.values.contains(s[i])) open.add(s[i]);
+      else if (m.keys.contains(s[i])) {
+        if (open.isNotEmpty && open.last == m[s[i]]) open.removeLast();
+        else return false;
+      }
+    }
+    return open.isEmpty;
+  }
 }
