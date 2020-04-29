9714bd2aa0cfb45ff208d07081921708959f137a
Sommano Phivilay <sommano@gmail.com>
2020-04-29 12:01:33 -0500

    Update pascals_triangle.dart

--- dart/pascals-triangle/lib/pascals_triangle.dart
+++ dart/pascals-triangle/lib/pascals_triangle.dart
@@ -1,3 +1,14 @@
 class PascalsTriangle {
-  // Put your code here
-}
+  List<List<int>> rows(int requiredRows){
+    List<List<int>> result = [];
+    List<int> prevRow = [];
+    for(int rowNr=1; rowNr<=requiredRows; rowNr++){
+      List<int> row = [1];
+      for(int pos = 1; pos < rowNr; pos++){
+        row.add(prevRow[pos - 1] + (prevRow.length > pos ? prevRow[pos]  : 0));
+      }
+      result.add(row);
+      prevRow = row;
+    }
+    return result;
+  }}
