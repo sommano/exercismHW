761060f2bb5a10ea2f7e320282ec00d4f1b2d81c
Sommano Phivilay <sommano@gmail.com>
2020-04-30 12:13:12 -0500

    Update phone_number.dart

--- dart/phone-number/lib/phone_number.dart
+++ dart/phone-number/lib/phone_number.dart
@@ -1,3 +1,22 @@
 class PhoneNumber {
-  // Put your code here
+  String clean(String number) {
+    number = removePermittedPunctuation(number);
+
+    if (!isNumeric(number)) return null;
+
+    if (number.length==11 && number.startsWith('1')) number = number.substring(1);
+
+    if (number.length!=10) return null;
+
+    if (['0', '1'].contains(number[0]) || ['0', '1'].contains(number[3])) return null;
+
+    return number;
+  }
+
+  String removePermittedPunctuation(String str) => str.split('').where((ch)=>
+      ![".", " ", "(", ")", "-", "+"].contains(ch)).fold("", (str, elem)=>str+elem);
+
+  bool isDigit(String ch) => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(ch);
+
+  bool isNumeric(String str) => str.split('').every(isDigit);
 }
