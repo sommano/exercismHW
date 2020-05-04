f40396e04231e3e880921c7bf0a1554083a0cb83
Sommano Phivilay <sommano@gmail.com>
2020-05-03 20:19:23 -0500

    Update prime_factors.dart

--- dart/prime-factors/lib/prime_factors.dart
+++ dart/prime-factors/lib/prime_factors.dart
@@ -1,3 +1,23 @@
 class PrimeFactors {
-  // Put your code here
-}
+  List<int> factors(int num){
+    List<int> result=[];
+
+    if(num<2){
+      return [];
+    }
+    else if(num==2 || num ==3 ){
+      return [num];
+    }
+    else{
+      int temp = num;
+      for(int i=2; i<num-1; i++){
+
+        while(temp%i==0){
+          result.add(i);
+          temp = temp~/i;
+
+        }
+      }
+      return result;
+    }
+  }}
