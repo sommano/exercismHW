324f7beb3d0d1374ed4ab651fb9cf20768bc28ec
Sommano Phivilay <sommano@gmail.com>
2020-05-06 12:11:47 -0500

    Update raindrops.dart

--- dart/raindrops/lib/raindrops.dart
+++ dart/raindrops/lib/raindrops.dart
@@ -1,3 +1,12 @@
 class Raindrops {
-  // Put your code here
+String convert(int given) {
+    StringBuffer out = new StringBuffer("");
+
+    (given%3 == 0) ? out.write("Pling") : out.write("");
+    (given%5 == 0) ? out.write("Plang") : out.write("");
+    (given%7 == 0) ? out.write("Plong") : out.write("");
+    (given%3 != 0 && given%5 != 0 && given%7 != 0) ? out.write(given) : out.write("");
+
+    return out.toString();
+  }
 }
