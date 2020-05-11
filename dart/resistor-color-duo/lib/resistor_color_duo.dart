a8e426e9bfd5f461e6d0b0c23a08c5bb38da3cae
Sommano Phivilay <sommano@gmail.com>
2020-05-10 19:53:28 -0500

    Update resistor_color_duo.dart

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,19 @@
 class ResistorColorDuo {
-  // Put your code here
+final colors = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ];
+
+  int colorValue(String c) => colors.indexOf(c);
+
+  int value(List<String> color_list) =>
+      10 * colorValue(color_list[0]) + colorValue(color_list[1]);
 }
