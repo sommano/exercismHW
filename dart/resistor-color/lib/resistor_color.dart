cdc380a0c90fc39cfd99a1e87fcf386fb9c11358
Sommano Phivilay <sommano@gmail.com>
2020-05-11 14:21:34 -0500

    Update resistor_color.dart

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,20 @@
 class ResistorColor {
-  // Put your code here
-}
+  final Map<String, int> resitorCodes = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+  };
+
+  List<String> get colors => resitorCodes.keys.toList();
+  
+
+  int colorCode(String color) {
+    return resitorCodes[color];
+  }}
