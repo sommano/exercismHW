0a88dd6976cb6056406ab241dd1ae934272b0830
Sommano Phivilay <sommano@gmail.com>
2020-05-13 13:12:03 -0500

    Update rna_transcription.dart

--- dart/rna-transcription/lib/rna_transcription.dart
+++ dart/rna-transcription/lib/rna_transcription.dart
@@ -1,3 +1,11 @@
 class RnaTranscription {
-  // Put your code here
-}
+  String toRna(String dna) {
+    RegExp exp = new RegExp(r"[^ACGT]");
+    RegExpMatch check = exp.firstMatch(dna);
+    if (check == null) {
+      Map<String, String> transcript = {"G": "C", "C": "G", "T": "A", "A": "U"};
+      String decode = dna.split("").map((f) => transcript[f]).toList().join("");
+      return decode;
+    } else
+      throw ArgumentError('Incorrect DNA');
+  }}
