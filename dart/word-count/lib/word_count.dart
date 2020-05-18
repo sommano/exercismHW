04ebcd02771780c9c1ad20904e71cb822b6597be
Sommano Phivilay <sommano@gmail.com>
2020-05-17 19:29:57 -0500

    Update word_count.dart

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,3 +1,19 @@
 class WordCount {
-  // Put your code here
+Map<String, int> countWords(String sentence) {
+    var words = new RegExp(r"\w+(\'\w+)?");
+
+    return words
+        .allMatches(sentence)
+        .map((item) => item.group(0).toLowerCase())
+        .fold(new Map<String, int>(),
+            (Map<String, int> wordCounts, String word) {
+      if (wordCounts.containsKey(word)) {
+        wordCounts[word] += 1;
+      } else {
+        wordCounts[word] = 1;
+      }
+      return wordCounts;
+    });
+    // ugly dartfmt tool
+  }
 }
