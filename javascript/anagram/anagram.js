559e5d84dcad8b5be3f7d1f1238dda9f13727f71
Sommano Phivilay <sommano@gmail.com>
2020-05-21 21:03:34 -0400

    Update anagram.js

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,8 +1,48 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+var anagram = function(word) {
 
-export const findAnagrams = () => {
-  throw new Error("Remove this statement and implement this function");
-}
+  var find_anagrams = function(possible_anagrams) {
+
+    // fails second last test
+
+    var true_anagarams = [];
+
+    if (possible_anagrams.constructor !== Array) {
+      possible_anagrams = possible_anagrams.split(" ");
+    }
+
+    for (i=0; i<possible_anagrams.length; i++) {
+      console.log()
+      possible_anagram = possible_anagrams[i].toLowerCase();
+
+      if (word == possible_anagram) { break; }
+       else if (word.length == possible_anagram.length){
+        // If words are of same length
+        for(j=0; j<word.length; j++) {
+          word = word.toLowerCase();
+          // master word on left, for each matching character, cut it out from word on the right.
+          // anagram means on the right you'd be left with an empty array, all characters slashed out
+          if (possible_anagram.indexOf(word[j]) != -1) {
+            possible_anagram = possible_anagram.slice(0, possible_anagram.indexOf(word[j])) + possible_anagram.slice(possible_anagram.indexOf(word[j]) + 1);
+          } else {
+            break;
+          }
+        }
+
+        if (possible_anagram.length == 0) {
+          true_anagarams.push(possible_anagrams[i]);
+        }
+      }
+    }
+
+    return true_anagarams;
+  };
+
+  var public = {
+    matches: find_anagrams
+  };
+
+  return public;
+
+};
+
+module.exports = anagram;
