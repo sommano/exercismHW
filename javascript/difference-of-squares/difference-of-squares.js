751ac06b8839f53feb81f846f7113f21456b413e
Sommano Phivilay <sommano@gmail.com>
2020-05-25 10:59:06 -0400

    Update difference-of-squares.js

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,7 +1,4 @@
-//
-// This is only a SKELETON file for the 'Difference of Squares' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
 
 export class Squares {
   constructor() {
